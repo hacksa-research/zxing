6ae076a7cc8eadac8ba26b74631d01ead361064b
Anson Ng <914anson5@gmail.com>
2025-07-29 12:56:08 +0800

    [merge-1](ZXI-3) enable kotlin compilation capability
    * [build](ZXI-3) add Kotlin dependencies and configuring the Kotlin Maven plugin
    
    * [test](ZXI-3) add Kotlin compilation test case and configure Maven plugin for Kotlin tests
    
    * [refactor](ZXI-3) update Kotlin version in pom.xml and fix formatting in KotlinCompilationTestCase
    
    * [build](ZXI-3) set test scope for kotlin-test-junit dependency in pom.xml

---
+++ core/src/test/kotlin/com/google/zxing/KotlinCompilationTestCase.kt
@@ -0,0 +1,18 @@
+package com.google.zxing
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+/**
+ * This is a simple test case to make sure that Kotlin is properly integrated in this library.
+ * It includes a trivial test that checks if 1 + 2 equals 3.
+ */
+class KotlinCompilationTestCase {
+
+  @Test
+  fun trivialTest() {
+    val a = 1
+    val b = 2
+    assertEquals(3, a + b, "1 + 2 should equal 3")
+  }
+}
